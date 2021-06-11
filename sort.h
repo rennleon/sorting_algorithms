4ca3dde544ddbe714f2cbe2655aa20e962d9aea3
renatoleon <2829@holbertonschool.com>
2021-06-11 23:11:43 +0000

    Add selection_sort prototype

--- sort.h
+++ sort.h
@@ -22,4 +22,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* Sorting functions */
+void selection_sort(int *array, size_t size);
+
 #endif /* SORT_H */
