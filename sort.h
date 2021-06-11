e9c77b4224304c152e37c3dc5c96546b75756dd5
renatoleon <2829@holbertonschool.com>
2021-06-11 22:37:36 +0000

    Add function prototypes

--- sort.h
+++ sort.h
@@ -18,4 +18,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Function prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
 #endif /* SORT_H */
