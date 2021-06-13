ceb2e6fea4615ef29c97d6326747a30be274f367
renatoleon <2829@holbertonschool.com>
2021-06-13 21:32:27 +0000

    Add swap function definition

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps two values inside an array
+ * @arr: Array of integers
+ * @i: Index 1
+ * @j: Index 2
+*/
+void swap(int *arr, int i, int j)
+{
+	int aux = arr[i];
+
+	arr[i] = arr[j];
+	arr[j] = aux;
+}
+
+/**
  * shell_sort - Sorts an array using shell sort algorithm with knuth's sequence
  * @array: Array of integers
  * @size: Array's size
