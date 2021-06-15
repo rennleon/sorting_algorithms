be7480144dcfe5c66fc35d4d16eb4660d2e386f2
renatoleon <2829@holbertonschool.com>
2021-06-15 17:07:55 +0000

    Add quick_sort_hoare prototype

--- sort.h
+++ sort.h
@@ -26,7 +26,7 @@ void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-/* Sorting functions (Aadvanced) */
+/* Sorting functions (Advanced) */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -34,6 +34,7 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 /* Utils */
 void swap(int *arr, int i, int j);
