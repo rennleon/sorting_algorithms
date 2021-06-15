7fd0102a7f4fc5f3400d0b3aed00344dd8f237f3
renatoleon <2829@holbertonschool.com>
2021-06-15 19:15:56 +0000

    Rrefactor code by creating a partition function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,8 +1,5 @@
 #include "sort.h"
 
-
-void solve_quick(int *array, size_t size, long lower, long upper);
-
 /**
  * swap - Swaps two values inside an array
  * @arr: Array of integers
@@ -18,35 +15,21 @@ void swap(int *arr, int i, int j)
 }
 
 /**
- * quick_sort - Sorts an array using quick sort algorithm
+ * partition - Divides an array into two parts and returns the partition index
  * @array: Array of integers
  * @size: Array's size
+ * @lower: Sub-array's lower index
+ * @upper: Sub-array's upper index
+ *
+ * Return: Last pivot position
 */
-void quick_sort(int *array, size_t size)
+int partition(int *array, size_t size, int lower, int upper)
 {
-	if (array == NULL || size <= 1)
-		return;
-
-	solve_quick(array, size, 0, size - 1);
-}
-
-/**
- * solve_quick - Sorts an array using quick sort
- * @array: Array of integers
- * @size: Array's size
- * @lower: Lower bound from wich the partitions starts
- * @upper: Upper bound on which the partition ends
-*/
-void solve_quick(int *array, size_t size, long lower, long upper)
-{
-	long i = lower - 1, j;
+	int i = lower - 1, j;
 	int pivot = array[upper];
 
-	if (lower >= upper)
-		return;
-
 	for (j = lower; j < upper; j++)
-		if (array[j] < pivot)
+		if (array[j] <= pivot)
 		{
 			if (++i == j)
 				continue;
@@ -61,6 +44,38 @@ void solve_quick(int *array, size_t size, long lower, long upper)
 		print_array(array, size);
 	}
 
-	solve_quick(array, size, lower, i - 1);
-	solve_quick(array, size, i + 1, upper);
+	return (i);
+}
+
+/**
+ * solve_quick - Sorts an array using quick sort
+ * @array: Array of integers
+ * @size: Array's size
+ * @lower: Lower bound from wich the partitions starts
+ * @upper: Upper bound on which the partition ends
+*/
+void solve_quick(int *array, size_t size, long lower, long upper)
+{
+	int pivot_pos;
+
+	if (lower >= upper)
+		return;
+
+	pivot_pos = partition(array, size, lower, upper);
+
+	solve_quick(array, size, lower, pivot_pos - 1);
+	solve_quick(array, size, pivot_pos + 1, upper);
+}
+
+/**
+ * quick_sort - Sorts an array using quick sort algorithm
+ * @array: Array of integers
+ * @size: Array's size
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size <= 1)
+		return;
+
+	solve_quick(array, size, 0, size - 1);
 }
